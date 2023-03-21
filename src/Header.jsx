223bbef4c6b5bd6a388cc34707577c612c93ae66
developerspack <developerspack23@gmail.com>
2023-03-21 13:59:20 +0300

    Simple

--- src/Header.jsx
+++ src/Header.jsx
@@ -28,39 +28,20 @@ const Header = () => {
         }`}
       >
         <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-          <div className="flex items-center gap-2">
-            <img src={logo} alt="logo" className="w-10 rounded-full h-10" />
-            <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-              Raro &nbsp;
-              <span className="sm:block hidden"> | Developers' Pack</span>
-            </p>
+          <div className="items-center">
+            <p className="text-white text-[18px] font-bold">Developers' Pack</p>
           </div>
 
           <ul className="list-none flex gap-10">
-            <li
-              className="text-[#aaa6c3]
-              hover:text-white text-[18px] font-medium cursor-pointer"
-            >
-              About
-            </li>
-            <li
-              className="text-[#aaa6c3]
-              hover:text-white text-[18px] font-medium cursor-pointer"
-            >
-              Work
-            </li>
-            <li
-              className="text-[#aaa6c3]
-              hover:text-white text-[18px] font-medium cursor-pointer"
-            >
-              Contact
-            </li>
+            <li className="text-[#aaa6c3] text-[18px] font-medium">About</li>
+            <li className="text-[#aaa6c3] text-[18px] font-medium">Work</li>
+            <li className="text-[#aaa6c3] text-[18px] font-medium">Contact</li>
           </ul>
         </div>
       </nav>
       {scrolled && (
         <div
-          className={`sm:px-16 px-6 w-full flex items-center py-1 fixed top-16 z-20 ${
+          className={`sm:px-16 px-6 w-full flex items-center py-1 fixed top-14 z-20 ${
             scrolled ? "bg-[#0c143b]" : "bg-transparent"
           }`}
         >
